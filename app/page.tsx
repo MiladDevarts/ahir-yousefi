833fcce63628ac5621b4636d30cd6ccd778a4323
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-21 13:46:42 +0330

    feat: add container detail

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-white font-mono">
-          Test Font (Azonix)
-      </h1>
-      <p className="text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, nemo quae et aliquam ad fugiat iure, dolor fuga vitae adipisci vero impedit error dolore deserunt laboriosam placeat molestiae porro consectetur!
-      </p>
+      <header>
+        <nav>
+
+        </nav>
+        <section>
+          
+        </section>
+      </header>
     </main>
   )
 }
