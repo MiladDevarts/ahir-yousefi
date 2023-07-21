9ae044ac579a4c146047daa8e07434e4ec0163c7
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-21 13:22:50 +0330

    feat: project setup & font configuration

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-white font-mono">
+          Test Font (Azonix)
+      </h1>
+      <p className="text-slate-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, nemo quae et aliquam ad fugiat iure, dolor fuga vitae adipisci vero impedit error dolore deserunt laboriosam placeat molestiae porro consectetur!
+      </p>
+    </main>
+  )
+}
