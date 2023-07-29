0c74bd260d0c0ed80f3218ebdc6a1db428940d30
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-30 01:24:51 +0330

    feat: initialize social-activity-x component

---
+++ components/SocialActivity/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const SocialActivity = () => {
+  return (
+    <section className='w-full bg-white py-10 my-10'>
+        <div className='container'>
+.
+        </div>
+    </section>
+  )
+}
+
+export default SocialActivity
