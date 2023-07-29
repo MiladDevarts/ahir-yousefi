a0e9f87eb1c33fc09049ebea170f8d22275c8298
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-29 13:14:01 +0330

    feat: add streaming platforms icon

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,7 +1,12 @@
 
+import AppleMusic from "./AppleMusic";
 import Facebook from "./Facebook";
 import Instagram from "./Instagram";
+import Soundcloud from "./Soundcloud";
+import Spotify from "./Spotify";
+import Telegram from "./Telegram";
 import TikTok from "./TikTok";
 import Twitter from "./Twitter";
+import Youtube from "./youtube";
 
-export {Facebook , Instagram, TikTok, Twitter}
+export { Facebook, Instagram, TikTok, Twitter, Spotify, Soundcloud, AppleMusic, Youtube, Telegram }
