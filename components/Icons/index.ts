6873c4aba061a56de00600f5b62cbc4a32a7a692
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-29 12:55:05 +0330

    feat: add icons component

---
+++ components/Icons/index.ts
@@ -0,0 +1,7 @@
+
+import Facebook from "./Facebook";
+import Instagram from "./Instagram";
+import TikTok from "./TikTok";
+import Twitter from "./Twitter";
+
+export {Facebook , Instagram, TikTok, Twitter}
