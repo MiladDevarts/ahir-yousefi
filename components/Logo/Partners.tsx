dbc583fcdffc39977aa1d75ae15066787a6e568f
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-31 17:45:31 +0330

    frat: add partners logo svgs

---
+++ components/Logo/Partners.tsx
@@ -0,0 +1,7 @@
+
+import Universal from "./Universal";
+import BlankerCords from "./BlankerCords";
+import VirginRecords from "./VirginRecords";
+import Atlantic from "./Atlantic";
+
+export { Universal, BlankerCords, VirginRecords, Atlantic}
