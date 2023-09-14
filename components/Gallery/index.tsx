d4b8965550575f7383ba6f82b7078ba6d912cccd
Milad Mohamadi <MiladDevarts@gmail.com>
2023-09-14 23:08:01 +0400

    fix: edit spacing/sizing

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -30,6 +30,7 @@ const Gallery = () => {
                 <div data-aos="fade-up" data-aos-duration="1000" className='rounded-[30px] w-full h-[150px] lg:h-[350px] bg-white row-start-2 row-end-3 col-start-1 col-end-3 lg:row-start-1 lg:row-end-2 lg:col-start-5 lg:col-end-10 bg-cover bg-center' style={{ backgroundImage: `url('/images/gallery/2.jpg')` }}></div>
                 <div data-aos="fade-left" data-aos-duration="1000" className='rounded-[30px] w-full h-[150px] lg:h-[350px] bg-white row-start-2 row-end-3 col-strat-3 col-end-4 lg:row-start-1 lg:row-end-2 lg:col-start-10 lg:col-end-13 bg-cover bg-center' style={{ backgroundImage: `url('/images/gallery/4.jpg')` }}></div>
                 <div data-aos="fade-down" data-aos-duration="1000" className='rounded-[30px] w-full h-[150px] lg:h-[350px] bg-white row-start-3 row-end-4 col-start-1 col-end-4 lg:row-start-2 lg:row-end-3 lg:col-span-12 bg-cover bg-center' style={{ backgroundImage: `url('/images/gallery/3.jpg')` }}></div>
+                <div data-aos="fade-down" data-aos-duration="1000" className='rounded-[30px] w-full h-[150px] lg:h-[350px] bg-white row-start-3 row-end-4 col-start-1 col-end-4 lg:row-start-2 lg:row-end-3 lg:col-span-12 bg-cover bg-center' style={{ backgroundImage: `url('/images/gallery/5.jpg')` }}></div>
             </div>
         </section>
     )
