5b6da86415758691f8d3dab126052f99a2fb055a
Milad Mohamadi <MiladDevarts@gmail.com>
2023-09-15 00:54:37 +0400

    fix: replace header bg/image on mb

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -4,10 +4,17 @@ import Hero from "../../Hero"
 
 const Header = () => {
     return (
-        <header className="w-full max-h-[120vh] bg-no-repeat bg-center bg-cover pb-48" style={{ backgroundImage: `url('/images/header/background.jpg')` }}>
-            <Navigation />
-            <Hero />
-        </header>
+        <>
+            <header className="hidden lg:flex flex-col w-full max-h-[120vh] bg-no-repeat bg-center bg-cover pb-48" style={{ backgroundImage: `url('/images/header/background.jpg')` }}>
+                <Navigation />
+                <Hero />
+            </header>
+
+            <header className="flex lg:hidden flex-col w-full max-h-[120vh] bg-no-repeat bg-center bg-cover pb-48" style={{ backgroundImage: `url('/images/header/background-mb.jpg')` }}>
+                <Navigation />
+                <Hero />
+            </header>
+        </>
     )
 }
 
