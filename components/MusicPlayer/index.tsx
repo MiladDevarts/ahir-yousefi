40fedd84322bab4712165c4157f7b527e3e9b031
Milad Mohamadi <MiladDevarts@gmail.com>
2024-05-11 10:59:07 +0400

    fix: remove deprecated attr

--- components/MusicPlayer/index.tsx
+++ components/MusicPlayer/index.tsx
@@ -25,8 +25,6 @@ const MusicPlayer = () => {
               src="https://open.spotify.com/embed/track/3HMY0r2BAdpasXMY8rseR0?utm_source=generator&theme=0"
               width="100%"
               height="152"
-              frameBorder="0"
-              allowfullscreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
             ></iframe>
@@ -35,8 +33,6 @@ const MusicPlayer = () => {
               src="https://open.spotify.com/embed/track/6JNJERZGJwDVgkmbohBw7u?utm_source=generator&theme=0"
               width="100%"
               height="152"
-              frameBorder="0"
-              allowfullscreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
             ></iframe>
@@ -46,8 +42,6 @@ const MusicPlayer = () => {
               src="https://open.spotify.com/embed/artist/4463nfFMmK1cwAWBQDwT5e?utm_source=generator&theme=0"
               width="100%"
               height="352"
-              frameBorder="0"
-              allowfullscreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
             ></iframe>
