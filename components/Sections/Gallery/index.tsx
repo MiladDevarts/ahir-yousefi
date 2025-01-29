f30598e14cf9a23dea8efe69f44414a9588aef28
Milad Mohamadi <MiladDevarts@gmail.com>
2025-01-29 23:17:18 +0400

    refactor: gallery section

--- components/Sections/Gallery/index.tsx
+++ components/Sections/Gallery/index.tsx
@@ -1,37 +1,69 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react'
-
-import Aos from 'aos'
+import React, { useEffect } from "react";
+import Aos from "aos";
 import "aos/dist/aos.css";
 
-const Gallery = () => {
-
-    useEffect(() => {
-        Aos.init({
-            easing: "ease-out-cubic",
-            once: true,
-            offset: 200,
-        });
-    }, []);
-
-    useEffect(() => {
-        Aos.refresh()
-    }, [])
-
-
-    return (
-        <section className='container my-44'>
-            <h2 className='mb-10 font-mono text-4xl'>
-                Gallery
-            </h2>
-            <div className='grid grid-cols-3 lg:grid-cols-12 grid-rows-2 lg:grid-rows-1 gap-x-7 gap-y-6'>
-                <div data-aos="fade-right" data-aos-duration="1000" className='rounded-[30px] w-full h-[250px] lg:h-[350px] bg-white row-start-1 row-end-2 col-start-1 col-end-3 lg:row-start-1 lg:row-end-2 lg:col-start-1 lg:col-end-5 bg-cover bg-center  transition-[background-size]' style={{ backgroundImage: `url('/images/gallery/1.jpg')` }}></div>
-                <div data-aos="fade-up" data-aos-duration="1000" className='rounded-[30px] w-full h-[250px] lg:h-[350px] bg-white row-start-1 row-end-2 col-start-3 col-end-4 lg:row-start-1 lg:row-end-2 lg:col-start-5 lg:col-end-10 bg-cover bg-center' style={{ backgroundImage: `url('/images/gallery/2.jpg')` }}></div>
-                <div data-aos="fade-left" data-aos-duration="1000" className='rounded-[30px] w-full h-[250px] lg:h-[350px] bg-white row-start-2 row-end-3 col-start-1 col-end-2 lg:row-start-1 lg:row-end-2 lg:col-start-10 lg:col-end-13 bg-cover bg-center' style={{ backgroundImage: `url('/images/gallery/3.jpg')` }}></div>
-            </div>
-        </section>
-    )
+interface GalleryItemProps {
+  imageUrl: string;
+  animation: "fade-right" | "fade-up" | "fade-left";
+  className: string;
 }
 
-export default Gallery
+const GalleryItem = ({ imageUrl, animation, className }: GalleryItemProps) => (
+  <div
+    data-aos={animation}
+    data-aos-duration="1000"
+    className={`rounded-[30px] w-full h-[250px] lg:h-[350px] bg-cover bg-center ${className}`}
+    style={{ backgroundImage: `url('${imageUrl}')` }}
+  />
+);
+
+const galleryItems = [
+  {
+    imageUrl: "/images/gallery/1.jpg",
+    animation: "fade-right" as const,
+    className:
+      "row-start-1 row-end-2 col-start-1 col-end-3 lg:row-start-1 lg:row-end-2 lg:col-start-1 lg:col-end-5",
+  },
+  {
+    imageUrl: "/images/gallery/2.jpg",
+    animation: "fade-up" as const,
+    className:
+      "row-start-1 row-end-2 col-start-3 col-end-4 lg:row-start-1 lg:row-end-2 lg:col-start-5 lg:col-end-10",
+  },
+  {
+    imageUrl: "/images/gallery/3.jpg",
+    animation: "fade-left" as const,
+    className:
+      "row-start-2 row-end-3 col-start-1 col-end-2 lg:row-start-1 lg:row-end-2 lg:col-start-10 lg:col-end-13",
+  },
+];
+
+const Gallery = () => {
+  useEffect(() => {
+    Aos.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 200,
+    });
+  }, []);
+
+  return (
+    <section className="container my-44">
+      <h2 className="mb-8 font-display font-bold text-[40px]">Gallery</h2>
+      <div className="grid grid-cols-3 lg:grid-cols-12 grid-rows-2 lg:grid-rows-1 gap-x-7 gap-y-6">
+        {galleryItems.map((item, index) => (
+          <GalleryItem
+            key={index}
+            imageUrl={item.imageUrl}
+            animation={item.animation}
+            className={item.className}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
