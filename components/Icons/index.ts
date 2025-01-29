1f3b51703367dc034321b29dd682b49e04df1bf7
Milad Mohamadi <MiladDevarts@gmail.com>
2025-01-29 20:55:17 +0400

    fix: craete componenet for AdvancedPlay icon

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,4 +1,3 @@
-
 import AppleMusic from "./AppleMusic";
 import Facebook from "./Facebook";
 import Instagram from "./Instagram";
@@ -9,5 +8,18 @@ import TikTok from "./TikTok";
 import Twitter from "./Twitter";
 import Youtube from "./Youtube";
 import Play from "./play";
+import advancedPlay from "./advancedPlay";
 
-export { Facebook, Instagram, TikTok, Twitter, Spotify, Soundcloud, AppleMusic, Youtube, Telegram, Play }
+export {
+  Facebook,
+  Instagram,
+  TikTok,
+  Twitter,
+  Spotify,
+  Soundcloud,
+  AppleMusic,
+  Youtube,
+  Telegram,
+  Play,
+  advancedPlay,
+};
