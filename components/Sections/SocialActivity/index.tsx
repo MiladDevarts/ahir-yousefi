d4ca0c375adc4a30f319a541d852293559228a99
Milad Mohamadi <MiladDevarts@gmail.com>
2025-01-29 23:09:09 +0400

    fix: minor: social-activity edits

--- components/Sections/SocialActivity/index.tsx
+++ components/Sections/SocialActivity/index.tsx
@@ -1,35 +1,41 @@
-
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const SocialActivity = () => {
   return (
-    <section className='w-full bg-white py-10 my-44'>
-      <div className='container flex flex-col lg:flex-row lg:items-center lg:justify-between gap-y-6'>
-        <div className='w-full lg:w-1/3 flex items-center gap-x-6 lg:gap-x-10'>
-          <Image src={"/images/social-activity/profile.png"} className='w-32 lg:w-60' width={400} height={400} alt='ahir yousefi, ahir' />
-          <div className='flex flex-col gap-y-3 text-black'>
-            <h3 className='text-2xl font-mono'>Ahir yousefi</h3>
-            <div className='flex items-center gap-x-3'>
-              <span className='text-xl font-mono'>INSTAGRAM</span>
+    <section className="w-full bg-black py-10 my-44">
+      <div className="container flex flex-col lg:flex-row lg:items-center lg:justify-between gap-y-6">
+        <div className="w-full lg:w-1/3 flex items-center gap-x-6 lg:gap-x-10">
+          <Image
+            src={"/images/social-activity/profile.png"}
+            className="w-32 lg:w-60"
+            width={400}
+            height={400}
+            alt="P91"
+          />
+          <div className="flex flex-col  text-white">
+            <h3 className="text-[32px] font-bold">Follow ME</h3>
+            <div className="flex items-center gap-x-3">
+              <span className="italic">iNSTAGRAM</span>
             </div>
-            <Link href={"#"} className='rounded-full block bg-black px-5 py-3 w-2/3 text-white text-center text-sm mt-2 mb-10'>
-              Follow
-            </Link>
           </div>
         </div>
-        <div className='w-full lg:w-1/2'>
-          <p className='text-[#7A7A7A] text-lg max-w-xl'>
-            Ahir Yousefi, born in Tehran on August 10th, 1997, embarked on his musical journey at the age of 16. His strong passion for music led him to seamlessly transition from a background in electrical engineering to pursue education in music at the Tehran University of Art.
+        <div className="w-full lg:w-1/2">
+          <p className="text-[#7A7A7A] text-lg max-w-xl">
+            Ahir Yousefi, born in Tehran on August 10th, 1997, embarked on his
+            musical journey at the age of 16. His strong passion for music led
+            him to seamlessly transition from a background in electrical
+            engineering to pursue education in music at the Tehran University of
+            Art.
             <br />
             <br />
-            Today, Ahir Yousefi is renowned as a songwriter, composer, musician, and accomplished singer.
+            Today, Ahir Yousefi is renowned as a songwriter, composer, musician,
+            and accomplished singer.
           </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SocialActivity
+export default SocialActivity;
