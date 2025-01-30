f54b0c6ce55c69d4a8ba6589c1a3df491d0b515f
Hamidreza Firouzi <hamidreza4dev@gmail.com>
2025-01-30 03:54:09 +0330

    fix: header

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <header
-        className="hidden lg:flex flex-col w-full max-h-[1000px] h-screen bg-no-repeat bg-center bg-cover pb-48"
+        className="hidden lg:flex flex-col w-full max-h-[1000px] h-screen bg-no-repeat bg-center bg-cover lg:pb-48"
         style={{ backgroundImage: `url('/images/header/background.jpg')` }}
       >
         <Navigation />
@@ -13,7 +13,7 @@ const Header = () => {
       </header>
 
       <header
-        className="flex lg:hidden flex-col w-full max-h-[120vh] bg-no-repeat bg-[10%_50%] bg-[size:240%_100%] pb-48"
+        className="flex lg:hidden flex-col w-full max-h-[1000px] h-screen bg-no-repeat bg-[10%_50%] bg-[size:240%_100%] pb-12 lg:pb-48"
         style={{ backgroundImage: `url('/images/header/background.jpg')` }}
       >
         <Navigation />
