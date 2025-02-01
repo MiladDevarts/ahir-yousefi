8f6fa4327e93434c4ba9fa5782ce0c76181a0fc7
Milad Mohamadi <MiladDevarts@gmail.com>
2025-02-01 12:52:38 +0400

    fix: change listen-now button height and parent width

--- components/Sections/Tracks/Card.tsx
+++ components/Sections/Tracks/Card.tsx
@@ -10,12 +10,12 @@ interface NewTrackProps {
 
 const Card: React.FC<NewTrackProps> = (props) => {
   return (
-    <div className="relative flex flex-col gap-6">
+    <div className="relative flex flex-col gap-7">
       <div
         className="rounded-3xl w-full h-[428px] bg-center bg-cover"
         style={{ backgroundImage: `url(${props.image})` }}
       ></div>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col">
         <h3 className="text-white font-sand text-[22px]">{props.title}</h3>
         <span className="text-[#CBCBCB]">
           Release Date : {props.release_date}
@@ -23,7 +23,7 @@ const Card: React.FC<NewTrackProps> = (props) => {
       </div>
 
       <div className="max-w-[14.125rem] w-full">
-        <Button href={props.url} className="w-full h-9 text-xs">
+        <Button href={props.url} className="w-full h-[44px] text-xs">
           Listen Now
         </Button>
       </div>
